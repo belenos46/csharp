1c06b7ad4f373694279f7b33ac4507b5ff07f066
Michael McPherson <michael@mpmcpherson.com>
2018-04-24 20:57:23 -0400

    initial commit

---
+++ registerSample/ConsoleRegisterApplication/ConsoleRegisterApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRegisterApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
